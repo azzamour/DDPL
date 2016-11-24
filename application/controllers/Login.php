9570dbd2870eba6836dc682ab5df228e2b0cccad
Muhammad Azzam <muhammadazzam1602@gmail.com>
2016-11-25 01:38:43 +0700

    Create Login.php

---
+++ application/controllers/Login.php
@@ -0,0 +1,41 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+class Login extends CI_Controller {
+    function __construct() {
+        parent::__construct();
+        $this->load->model('mod_login'); 
+    }
+    function index()
+    {
+        $session = $this->session->userdata('isLogin'); 
+        if($session == FALSE) 
+        {
+            $this->load->view('login-form');
+        }else 
+        {
+            redirect('dashboard');
+        }
+    }    
+    function autentifikasi()
+    {
+        $username = $this->input->post("username");
+        $password = $this->input->post("password");
+        
+        $cek = $this->mod_login->cek_user($username,md5($password));
+        if(count($cek) == 1){
+            foreach ($cek as $cek) {
+                $level = $cek['level'];
+            }
+            $this->session->set_userdata(array(
+                'isLogin'   => TRUE,
+                'username'  => $username,
+                'level'      => $level,
+            ));
+                
+            redirect('dashboard','refresh');
+        }else{
+            echo "<script>alert('Username atau Password salah!')</script>";
+            redirect('login','refresh');
+        }
+        
+    }
+}
