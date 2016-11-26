23549123097cefe723b2645e08f36aaceeb208e4
Oky Suryadi <okysuryadi@hotmail.com>
2016-11-26 07:03:54 +0700

    CodeIgniter 3.1.2
    application / config / config.php

--- application/config/config.php
+++ application/config/config.php
@@ -314,7 +314,7 @@ $config['cache_query_string'] = FALSE;
 | https://codeigniter.com/user_guide/libraries/encryption.html
 |
 */
-$config['encryption_key'] = 'ddplc16';
+$config['encryption_key'] = 'DDPLC2016';
 
 /*
 |--------------------------------------------------------------------------
